4f0b5bbbcc87d34604d5887f7cac8c299ce7f7a4
Kamilo Vasquez <killo.blanco@gmail.com>
2023-01-09 18:30:44 -0500

    Update index.tsx

--- src/components/top-bar/index.tsx
+++ src/components/top-bar/index.tsx
@@ -1,16 +1,21 @@
 import Grid2 from '@mui/material/Unstable_Grid2'
 import React from 'react'
 import { Thumbnail } from './thumbnail'
-import { Button } from '@mui/material'
+import { Button, Typography } from '@mui/material'
 import { Icon } from '@iconify/react'
 import { ThemeToggle } from '../theme-toggle'
 
 export const TopBar = () => (
-  <Grid2 container spacing={2} alignItems="center" sx={{ mb: 4 }}>
+  <Grid2 container spacing={2} alignItems="center" justifyContent="space-evenly" sx={{ mb: 4 }}>
     <Grid2 xs="auto">
       <Thumbnail />
     </Grid2>
-    <Grid2 xs textAlign="right">
+    <Grid2 xs={9} sm="auto">
+      <Typography variant="h3">
+        Kamilo Vasquez
+      </Typography>
+    </Grid2>
+    <Grid2 xs={9} sm textAlign="right">
       <Button
         component="a"
         href="//kamilo.dev"
@@ -21,7 +26,7 @@ export const TopBar = () => (
         Acerca de mi
       </Button>
     </Grid2>
-    <Grid2 xs="auto">
+    <Grid2 xs={2} sm="auto">
       <ThemeToggle />
     </Grid2>
   </Grid2>
