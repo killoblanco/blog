22197aefa8d691692787280b84066d2a4cbf3c89
Kamilo Vasquez <killo.blanco@gmail.com>
2023-01-08 20:08:02 -0500

    Create index.tsx

---
+++ src/components/side-bar/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Stack, Typography } from '@mui/material'
+
+export const SideBar = () => (
+  <Stack spacing={2}>
+    <Typography variant="h3">
+      Sidebar
+    </Typography>
+  </Stack>
+)
