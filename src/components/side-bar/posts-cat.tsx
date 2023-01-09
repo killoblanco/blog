b159af717ed72feb3325e0d56c80606a03c56d43
Kamilo Vasquez <killo.blanco@gmail.com>
2023-01-09 16:43:11 -0500

    Create posts-cat.tsx

---
+++ src/components/side-bar/posts-cat.tsx
@@ -0,0 +1,10 @@
+import { Stack, Typography } from '@mui/material'
+import React from 'react'
+
+export const PostsCategories = () => (
+  <Stack spacing={2}>
+    <Typography variant="subtitle1">
+      Artículos por temas
+    </Typography>
+  </Stack>
+)
