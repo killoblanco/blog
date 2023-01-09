1b9e6aa76fed18555280cc8132c05d4bc1eb9333
Kamilo Vasquez <killo.blanco@gmail.com>
2023-01-09 18:30:38 -0500

    Update palette.ts

--- src/theme/palette.ts
+++ src/theme/palette.ts
@@ -4,40 +4,35 @@ import { argbFromHex, hexFromArgb, Scheme, themeFromSourceColor } from '@materia
 const { schemes, palettes } = themeFromSourceColor(argbFromHex('#C7F598'))
 const getColorHex = (mode: 'light' | 'dark') => (color: keyof Scheme) => hexFromArgb(schemes[mode][color] as number)
 
-export const paletteColor = (mode: 'light' | 'dark'): Partial<ThemeOptions['palette']> => {
-  console.log({
-    primary: getColorHex(mode)('primaryContainer')
-  })
-  return ({
-    mode,
-    common: {
-      black: hexFromArgb(palettes.primary.tone(10)),
-      white: hexFromArgb(palettes.primary.tone(90))
-    },
-    primary: {
-      light: getColorHex(mode)('primaryContainer'),
-      main: getColorHex(mode)('primary'),
-      dark: getColorHex(mode)('onPrimaryContainer')
-    },
-    secondary: {
-      light: getColorHex(mode)('secondaryContainer'),
-      main: getColorHex(mode)('secondary'),
-      dark: getColorHex(mode)('onSecondaryContainer')
-    },
-    error: {
-      light: getColorHex(mode)('errorContainer'),
-      main: getColorHex(mode)('error'),
-      dark: getColorHex(mode)('onErrorContainer')
-    },
-    info: {
-      light: getColorHex(mode)('tertiaryContainer'),
-      main: getColorHex(mode)('tertiary'),
-      dark: getColorHex(mode)('onTertiaryContainer')
-    },
-    divider: getColorHex(mode)('outline'),
-    background: {
-      default: getColorHex(mode)('surface'),
-      paper: getColorHex(mode)('surfaceVariant')
-    }
-  })
-}
+export const paletteColor = (mode: 'light' | 'dark'): Partial<ThemeOptions['palette']> => ({
+  mode,
+  common: {
+    black: hexFromArgb(palettes.primary.tone(10)),
+    white: hexFromArgb(palettes.primary.tone(90))
+  },
+  primary: {
+    light: getColorHex(mode)('primaryContainer'),
+    main: getColorHex(mode)('primary'),
+    dark: getColorHex(mode)('onPrimaryContainer')
+  },
+  secondary: {
+    light: getColorHex(mode)('secondaryContainer'),
+    main: getColorHex(mode)('secondary'),
+    dark: getColorHex(mode)('onSecondaryContainer')
+  },
+  error: {
+    light: getColorHex(mode)('errorContainer'),
+    main: getColorHex(mode)('error'),
+    dark: getColorHex(mode)('onErrorContainer')
+  },
+  info: {
+    light: getColorHex(mode)('tertiaryContainer'),
+    main: getColorHex(mode)('tertiary'),
+    dark: getColorHex(mode)('onTertiaryContainer')
+  },
+  divider: getColorHex(mode)('outline'),
+  background: {
+    default: getColorHex(mode)('surface'),
+    paper: getColorHex(mode)('surfaceVariant')
+  }
+})
