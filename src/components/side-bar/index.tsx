423c2465016d75161447c66170498f5dee5c5932
Kamilo Vasquez <killo.blanco@gmail.com>
2023-01-09 16:43:16 -0500

    Update index.tsx

--- src/components/side-bar/index.tsx
+++ src/components/side-bar/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Stack, Typography } from '@mui/material'
+import { Stack } from '@mui/material'
+import { SocialNetwork } from './social-network'
+import { PostsCategories } from './posts-cat'
 
 export const SideBar = () => (
   <Stack spacing={2}>
-    <Typography variant="h3">
-      Sidebar
-    </Typography>
+    <SocialNetwork />
+    <PostsCategories />
   </Stack>
 )
