e3d03f1ffe8d4e2f270abdda40742e46ee30a952
Kamilo Vasquez <killo.blanco@gmail.com>
2023-01-09 19:26:25 -0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,20 @@
 import * as React from 'react'
 import type { HeadFC, PageProps } from 'gatsby'
-import { graphql, Link } from 'gatsby'
-import { Box, Stack, Typography } from '@mui/material'
+import { graphql } from 'gatsby'
+import { Stack } from '@mui/material'
 import { Seo } from '../components/seo/seo'
+import { PostCard } from '../components/post-card'
 
 interface DataType {
   allMdx: {
-    nodes: Array<{
-      id: string
-      excerpt: string
-      frontmatter: Frontmatter
-    }>
+    nodes: PostCardProps[]
   }
 }
 
 const IndexPage: React.FC<PageProps<DataType>> = ({ data }) => (
   <Stack component="main" spacing={2.5}>
-    {data.allMdx.nodes.map(({ id, excerpt, frontmatter }) => (
-      <Box key={id}>
-        <Typography
-          component={Link}
-          to={frontmatter.slug}
-          variant="h5"
-        >{frontmatter.title}</Typography>
-        <Typography>{excerpt}</Typography>
-        <Typography variant="body2">{frontmatter.date}</Typography>
-      </Box>
+    {data.allMdx.nodes.map(({ id, ...props }) => (
+      <PostCard key={id} {...props} />
     ))}
   </Stack>
 )
