39c4d4e984956f2b8b30e207e225439845961fdc
Kamilo Vasquez <killo.blanco@gmail.com>
2023-01-10 19:44:00 -0500

    Create reading-time.ts

---
+++ src/components/utils/reading-time.ts
@@ -0,0 +1,7 @@
+export const readingTime = (text: string) => {
+  const wordsPerMinute = 200
+  const noOfWords = text.split(/\s/g).length
+  const minutes = noOfWords / wordsPerMinute
+  const readTime = Math.ceil(minutes)
+  return `${readTime} minuto${readTime > 1 ? 's' : ''} de lectura`
+}
