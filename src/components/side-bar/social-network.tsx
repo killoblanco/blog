821e531561eebc157eb82c9b899a61266346fce5
Kamilo Vasquez <killo.blanco@gmail.com>
2023-01-14 11:39:06 -0500

    Update social-network.tsx

--- src/components/side-bar/social-network.tsx
+++ src/components/side-bar/social-network.tsx
@@ -5,10 +5,10 @@ import React from 'react'
 const NETWORKS = [
   { href: 'https://github.com/killoblanco', icon: 'github' },
   { href: 'https://www.linkedin.com/in/killoblanco', icon: 'linkedin' },
-  { href: 'https://www.facebook.com/killoblanco/', icon: 'facebook' },
-  { href: 'https://twitter.com/killoblanco', icon: 'twitter' },
-  { href: 'https://www.youtube.com/@killo.blanco?sub_confirmation=1', icon: 'youtube' },
-  { href: 'https://www.twitch.tv/killoblanco', icon: 'twitch' },
+  // { href: 'https://www.facebook.com/killoblanco/', icon: 'facebook' },
+  // { href: 'https://twitter.com/killoblanco', icon: 'twitter' },
+  // { href: 'https://www.youtube.com/@killo.blanco?sub_confirmation=1', icon: 'youtube' },
+  // { href: 'https://www.twitch.tv/killoblanco', icon: 'twitch' },
   { href: 'https://www.paypal.com/donate/?hosted_button_id=XPHPMQWVW2SWA', icon: 'paypal' }
 ]
 
