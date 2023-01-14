00a602794da21a209c159f0c7ae95727bd0fc272
Kamilo Vasquez <killo.blanco@gmail.com>
2023-01-14 09:53:30 -0500

    Update base.tsx

--- src/components/layouts/base.tsx
+++ src/components/layouts/base.tsx
@@ -3,21 +3,25 @@ import { Container, Theme } from '@mui/material'
 import { TopBar } from '../top-bar'
 import Grid2 from '@mui/material/Unstable_Grid2'
 import { SideBar } from '../side-bar'
+import { Footer } from '../footer'
 
 const flexOrder = (order: number) => (t: Theme) => ({
   [t.breakpoints.down('sm')]: { order }
 })
 
 export const BaseLayout = ({ children }: PropsWithChildren) => (
-  <Container maxWidth="lg" sx={{ my: 4 }}>
-    <TopBar />
-    <Grid2 container spacing={2}>
-      <Grid2 xs={12} md={9} sx={flexOrder(2)}>
-        {children}
+  <>
+    <Container maxWidth="lg" sx={{ my: 4 }}>
+      <TopBar />
+      <Grid2 container spacing={2}>
+        <Grid2 xs={12} md={9} sx={flexOrder(2)}>
+          {children}
+        </Grid2>
+        <Grid2 xs={12} md={3} sx={flexOrder(1)}>
+          <SideBar />
+        </Grid2>
       </Grid2>
-      <Grid2 xs={12} md={3} sx={flexOrder(1)}>
-        <SideBar />
-      </Grid2>
-    </Grid2>
-  </Container>
+    </Container>
+    <Footer />
+  </>
 )
