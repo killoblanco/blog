f91279eb29fad17583936d8d85afaad0d9ec9fee
Kamilo Vasquez <killo.blanco@gmail.com>
2023-01-14 11:38:50 -0500

    Update {mdx.fields__slug}.tsx

--- src/pages/{mdx.fields__slug}.tsx
+++ src/pages/{mdx.fields__slug}.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import type { HeadFC, PageProps } from 'gatsby'
+import { graphql } from 'gatsby'
 import { MDXProvider } from '@mdx-js/react'
-import { Divider, Paper, Table, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
+import {
+  Breadcrumbs,
+  Divider,
+  Paper,
+  Table,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography
+} from '@mui/material'
+import { readingTime } from '../components/utils/reading-time'
 
 const mdxComponents = {
-  p: (props: any) => <Typography variant="body1" sx={{ my: 3 }} {...props} />,
   a: (props: any) => (
     <Typography gutterBottom
                 {...props}
@@ -17,13 +28,6 @@ const mdxComponents = {
                 }}
     />
   ),
-  li: (props: any) => <Typography gutterBottom variant="body1" {...props} />,
-  h1: (props: any) => <Typography gutterBottom variant="h1" {...props} />,
-  h2: (props: any) => <Typography gutterBottom variant="h2" {...props} />,
-  h3: (props: any) => <Typography gutterBottom variant="h3" {...props} />,
-  h4: (props: any) => <Typography gutterBottom variant="h4" {...props} />,
-  h5: (props: any) => <Typography gutterBottom variant="h5" {...props} />,
-  h6: (props: any) => <Typography gutterBottom variant="h6" {...props} />,
   table: (props: any) => (
     <TableContainer component={Paper}>
       <Table variant="h6" {...props} />
@@ -35,17 +39,55 @@ const mdxComponents = {
   hr: (props: any) => <Divider {...props} variant="fullWidth" flexItem />
 }
 
-const PostPage: React.FC<PageProps> = ({ children, ...props }) => {
-  console.log({ props })
-  return (
-    <main>
-      <MDXProvider components={mdxComponents}>
-        {children}
-      </MDXProvider>
-    </main>
-  )
-}
+const PostPage: React.FC<PageProps<PostPageProps, PostPageContext>> = ({
+  children,
+  data: { allMdx: { nodes: [post] } },
+  pageContext
+}) => (
+  <main>
+    <Typography component="h1" variant="h3" gutterBottom>
+      {pageContext.frontmatter.title}
+    </Typography>
+    <Breadcrumbs separator="·" sx={{
+      mb: 3,
+      ol: {
+        justifyContent: 'flex-end'
+      }
+    }}>
+      <Typography variant="body2" textTransform="capitalize">{post.frontmatter.date}</Typography>
+      <Typography variant="body2">{readingTime(post.fields.timeToRead.minutes)}</Typography>
+    </Breadcrumbs>
+    <Divider variant="fullWidth" flexItem>
+      <Typography component="span" variant="h4">
+        &nbsp;&nbsp;🦄&nbsp;&nbsp;
+      </Typography>
+    </Divider>
+    <MDXProvider components={mdxComponents}>
+      {children}
+    </MDXProvider>
+  </main>
+)
 
 export const Head: HeadFC = () => <title />
+
+export const query = graphql`
+  query($id: String!) {
+    allMdx(filter: {id: {eq: $id}}) {
+      nodes {
+        frontmatter {
+          date(locale: "es-CO", formatString: "MMMM D, YYYY")
+        }
+        fields {
+          timeToRead {
+            minutes
+            text
+            time
+            words
+          }
+        }
+      }
+    }
+  }
+`
 
 export default PostPage
