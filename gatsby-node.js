ba3947edc186cf0debd015d530f261af4a8fe55d
Kamilo Vasquez <killo.blanco@gmail.com>
2023-01-14 11:41:02 -0500

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -17,3 +17,30 @@ exports.onCreateNode = ({ node, actions }) => {
     })
   }
 }
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { data } = await graphql(`
+    query {
+      allMdx {
+        edges {
+          node {
+            id
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  data.allMdx.edges.forEach(({ node: { id, fields } }) => {
+    actions.createPage({
+      path: fields.slug,
+      component: require.resolve('./src/pages/{mdx.fields__slug}.tsx'),
+      context: {
+        id
+      }
+    })
+  })
+}
