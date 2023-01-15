6b003ecb4ae2d506c3d408f9160dcd6ff4ab6245
Kamilo Vasquez <killo.blanco@gmail.com>
2023-01-15 18:54:46 -0500

    Update gatsby-config.ts

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,8 @@ import type { GatsbyConfig } from 'gatsby'
 const config: GatsbyConfig = {
   siteMetadata: {
     title: 'Blog | ',
-    siteUrl: 'https://blog.kamilo.dev'
+    siteUrl: 'https://blog.kamilo.dev',
+    description: 'I am a software developer with over 10 years of experience creating solutions that positively impact people\'s lives. My expertise includes web development, mobile apps, and desktop apps. I am a self-taught learner with an interest in AI. I am currently seeking job opportunities abroad that require relocation. I am a highly skilled and motivated professional looking to contribute to a company\'s success. Please feel free to contact me to discuss how I can help your team achieve its goals.'
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
