39377fb7fbb83fccb37a1772afaa0449682d75ee
Kamilo Vasquez <killo.blanco@gmail.com>
2023-01-15 18:56:34 -0500

    Update seo.tsx

--- src/components/seo/seo.tsx
+++ src/components/seo/seo.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import { useSiteMeta } from './use-site-meta'
 
-interface SeoProps {
+interface SeoProps extends Partial<Frontmatter> {
   title?: string
 }
 
-export const Seo = ({ title }: SeoProps) => {
+export const Seo = (frontmatter: SeoProps) => {
   const { site: { siteMetadata } } = useSiteMeta()
 
   return (
-    <title>{`${siteMetadata.title} ${title ?? 'Kamilo Dev'}`}</title>
+    <>
+      <title>{`${siteMetadata.title} ${frontmatter?.title ?? 'Kamilo Dev'}`}</title>
+      <meta name="author" content="Kamilo Vasquez | @killoblanco"/>
+      <meta name="description" content={frontmatter?.description ?? siteMetadata.description} />
+      {((frontmatter?.tags) != null) && <meta name="keywords" content={frontmatter.tags.join(', ')} />}
+    </>
   )
 }
