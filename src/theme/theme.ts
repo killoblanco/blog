ae57a2f9a794e86fe3a59362a64149f18309c45a
Kamilo Vasquez <killo.blanco@gmail.com>
2023-01-16 19:34:18 -0500

    Update theme.ts

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -6,50 +6,54 @@ import '@fontsource/readex-pro/700.css'
 import { createTheme, responsiveFontSizes, Theme } from '@mui/material'
 import { paletteColor } from './palette'
 
-export const theme = (darkMode: boolean): Theme => responsiveFontSizes(
-  createTheme({
-    components: {
-      MuiAppBar: {
-        styleOverrides: {
-          root: {
-            backgroundColor: 'transparent'
+export const theme = (darkMode: boolean): Theme => {
+  const palette = paletteColor(darkMode ? 'dark' : 'light')!
+
+  return responsiveFontSizes(
+    createTheme({
+      components: {
+        MuiAppBar: {
+          styleOverrides: {
+            root: {
+              backgroundColor: 'transparent'
+            }
           }
-        }
-      },
-      MuiButton: {
-        styleOverrides: {
-          root: {
-            textTransform: 'capitalize'
+        },
+        MuiButton: {
+          styleOverrides: {
+            root: {
+              textTransform: 'capitalize'
+            }
           }
-        }
-      },
-      MuiFab: {
-        styleOverrides: {
-          root: {
-            textTransform: 'capitalize'
+        },
+        MuiFab: {
+          styleOverrides: {
+            root: {
+              textTransform: 'capitalize'
+            }
           }
-        }
-      },
-      MuiDivider: {
-        styleOverrides: {
-          root: {
-            '&::before, &::after': {
-              borderColor: paletteColor(darkMode ? 'dark' : 'light').primary?.dark
+        },
+        MuiDivider: {
+          styleOverrides: {
+            root: {
+              '&::before, &::after': {
+                borderColor: palette?.primary?.dark
+              }
             }
           }
         }
+      },
+      palette: paletteColor(darkMode ? 'dark' : 'light'),
+      typography: {
+        fontFamily: 'Readex Pro, sans-serif',
+        fontWeightRegular: 300,
+        fontSize: 16,
+        h1: { fontWeight: 700 },
+        h2: { fontWeight: 600 },
+        h3: { fontWeight: 600 },
+        h4: { fontWeight: 500 },
+        h5: { fontWeight: 500 }
       }
-    },
-    palette: paletteColor(darkMode ? 'dark' : 'light'),
-    typography: {
-      fontFamily: 'Readex Pro, sans-serif',
-      fontWeightRegular: 300,
-      fontSize: 16,
-      h1: { fontWeight: 700 },
-      h2: { fontWeight: 600 },
-      h3: { fontWeight: 600 },
-      h4: { fontWeight: 500 },
-      h5: { fontWeight: 500 }
-    }
-  })
-)
+    })
+  )
+}
