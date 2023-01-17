224da75362955acacb60973d24903b3b760084a7
Kamilo Vasquez <killo.blanco@gmail.com>
2023-01-16 19:34:28 -0500

    Update posts-cat.tsx

--- src/components/side-bar/posts-cat.tsx
+++ src/components/side-bar/posts-cat.tsx
@@ -1,10 +1,12 @@
 import { Stack, Typography } from '@mui/material'
 import React from 'react'
+import { CategoryList } from './category-list'
 
 export const PostsCategories = () => (
   <Stack spacing={2}>
     <Typography variant="h5">
       Artículos por temas
     </Typography>
+    <CategoryList />
   </Stack>
 )
